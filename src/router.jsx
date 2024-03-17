1ce68f47e14949803ef353ebaa5eba24f22d6797
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-03-17 13:54:52 -0300

    feat: add react-router-dom dependecy

---
+++ src/router.jsx
@@ -0,0 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
+import { RootLayout} from './pages/RootLayout';
+import { Home } from './pages/Home';
+import {ItemsLayout} from './pages/ItemsLayout';
+import { ListItems } from './pages/ListItems';
+import { CreateItems } from './pages/CreateItems';
+import { ShowItems } from './pages/ShowItems';
+import { UpdateItems } from './pages/UptadeItems';
+
+export function MainRoutes() {
+    return (
+        <Routes>
+            <Route path="/" element={<RootLayout/>}>
+                <Route index element={<Home/>} />
+                <Route path="items" element={<ItemsLayout/>}>
+                    <Route index element={<ListItems />} /> 
+                    <Route path="new" element={<CreateItems />} />
+                    <Route path=":id" element={<ShowItems />} />
+                    <Route path=":id/update" element={<UpdateItems />} />
+                </Route>
+            </Route>
+            <Route path="*" element={<h1 className='text-white text-5xl text-center'>Not found</h1>} />
+        </Routes>
+    );
+}
