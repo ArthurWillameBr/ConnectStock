1519c0572c2cbf08b3a8c42bfa5d964833712e18
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-03-19 14:30:17 -0300

    org

---
+++ src/components/DeleteButton.jsx
@@ -0,0 +1,10 @@
+export function DeleteButton() {
+    return (
+        <button
+        className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
+        type="button"
+        >
+            Excluir
+        </button>
+    )
+}
